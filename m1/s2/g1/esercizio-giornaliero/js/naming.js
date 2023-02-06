0b70b06123acf5c6164c6cad9228b7a28eff788d
DennisLoiak11 <dennisloiak11@gmail.com>
2023-02-06 15:34:55 +0100

    s2/g1-con-esercizio

---
+++ m1/s2/g1/esercizio-giornaliero/js/naming.js
@@ -0,0 +1,20 @@
+var pet_preferito = 'gatto';
+console.log(pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+console.log(petPreferito);
+
+var PetPreferito = 'coniglio';
+console.log(PetPreferito);
+
+var $pet = 'giraffa';
+console.log($pet);
+
+var _pet = 'leone';
+console.log(_pet);
+
+var _pet2 = 'pantera';
+document.write(_pet2);
+
