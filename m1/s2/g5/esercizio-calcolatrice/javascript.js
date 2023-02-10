e286aefe0f0b823583339e06f923db216b216c92
DennisLoiak11 <dennisloiak11@gmail.com>
2023-02-10 12:13:30 +0100

    g5-calcolatrice

---
+++ m1/s2/g5/esercizio-calcolatrice/javascript.js
@@ -0,0 +1,14 @@
+function aggiungiSimbolo(elemento) {
+    //let simbolo = elemento.textContent
+    let simbolo = elemento.getAttribute('data-simbolo');
+
+    let display = document.querySelector('#display')
+    display.value += simbolo;
+
+}
+
+function totale() {
+    let display = document.querySelector('#display');
+
+    display.value = eval(display.value);
+}
